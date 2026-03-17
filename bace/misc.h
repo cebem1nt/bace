cb0a5a4ca39304cc3eeb5aa0e77575ca36fa0ab7
cebem1nt <mineewarik@gmail.com>
2026-03-17 20:24:28 -0300

    misc.h: added readfile + made error_exit macro acept all __VA_ARGS__

--- bace/misc.h
+++ bace/misc.h
@@ -1,8 +1,14 @@
 #ifndef _BACE_MISC_H
 #define _BACE_MISC_H
 
-#define error_exit(fmt, ...) do {           \
-    fprintf(stderr, fmt, __VA_ARGS__);      \
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define error_exit(...) do {                \
+    fprintf(stderr, __VA_ARGS__);           \
     exit(1);                                \
 } while(0)
 
@@ -11,4 +17,35 @@
     exit(1);                                \
 } while(0)
 
-#endif
+#ifdef BACE_IMPLEMENTATION
+char* readfile(const char* fp, size_t* out_size) 
+{
+    size_t fsize;
+    char*  out;
+    int    fd;
+    
+    if (open(fp, O_RDONLY) == -1) {
+        perror("open");
+        return NULL;   
+    }
+
+    fsize = lseek(fd, 0, SEEK_END);
+    lseek(fd, 0, SEEK_SET);
+
+    if ((out = (char*) malloc(fsize)) == NULL)
+        return NULL;
+
+    if (read(fd, out, fsize) == -1) {
+        perror("read");
+        free(out);
+        return NULL;
+    }
+
+    if (out_size)
+        *out_size = fsize;
+
+    return out;
+}
+
+#endif // BACE_IMPLEMENTATION
+#endif // _BACE_MISC_H
